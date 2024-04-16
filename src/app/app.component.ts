c903da97031f88836a29090b9cbac72c9fc166d9
seunrae <ademilua.oluseun@gmail.com>
2024-04-16 21:17:08 +0100

    commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//@ts-ignore
+import Typewriter from 't-writer.js'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-deploy';
+
+  ngOnInit(): void{
+    const target = document.querySelector('.tw')
+
+    const writer = new Typewriter(target, {
+      loop: true,
+      typeSpeed: 80,
+      deleteSpeed: 80,
+      typeColor: '#E7DDFA'
+    })
+    
+    writer
+      .type('$:Hello, my name is Murasaki_...')
+      .rest(500)
+      .changeOps({ deleteSpeed: 20 })
+      .remove(30)
+      .type('I am a Software Developer,')
+      .rest(500)
+      .remove(26)
+      .type('I can provide solutions to your business problems')
+      .rest(500)
+      .changeOps({ deleteSpeed: 20 })
+      .remove(49)
+      .type('feel free to reach out to me on any of my platforms')
+      .rest(500)
+      .clear()
+      .start()
+  }
 }
